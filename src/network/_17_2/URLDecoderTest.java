f47bbc5ccf4113dd28a364ea25407cd67fbc37c1
kaiyun <kaiyun@zillionfortune.com>
2017-11-10 14:24:01 +0800

    添加网络编程

---
+++ src/network/_17_2/URLDecoderTest.java
@@ -0,0 +1,25 @@
+package network._17_2;
+
+
+import java.net.*;
+/**
+ * Description:
+ */
+public class URLDecoderTest
+{
+	public static void main(String[] args)
+		throws Exception
+	{
+		// 将application/x-www-form-urlencoded字符串
+		// 转换成普通字符串
+		// 其中的字符串直接从图17.3所示窗口复制过来
+		String keyWord = URLDecoder.decode(
+			"%E7%96%AF%E7%8B%82java", "utf-8");
+		System.out.println(keyWord);
+		// 将普通字符串转换成
+		// application/x-www-form-urlencoded字符串
+		String urlStr = URLEncoder.encode(
+			"疯狂Android讲义" , "GBK");
+		System.out.println(urlStr);
+	}
+}
